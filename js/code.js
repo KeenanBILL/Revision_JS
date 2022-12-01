77c9d73458b39921bd81f1406c4cfab560bbdeb3
KeenanBILL <t19008@tsiba.ac.za>
2022-12-01 15:40:20 +0200

    Rev

--- js/code.js
+++ js/code.js
@@ -75,12 +75,152 @@
 
 // ====switch statement====
 
-let monthInt = 1;
-switch (monthInt) {
-    case 1:
-        console.log("January");
-    break;
-    case 2:
-        console.log("February")
-    break;
-}
+// let monthInt = 1;
+// switch (monthInt) {
+//     case 1:
+//         console.log("January");
+//     break;
+//     case 2:
+//         console.log("February")
+//     break;
+// }
+
+// let x = 5;
+
+// if (x === 10) {
+//     console.log ('something');
+// } else (x === 5){
+//     console.log ('anything');
+// }
+
+// let marks = 84;
+
+// switch (true) {
+//     case 100:
+
+//     console.log(`You have out done yourself`);
+
+//     break;
+
+//     case(marks >= 90 && marks < 100):
+
+//     console.log(`You have out done yourself 90 times over`);
+
+//     break;
+
+//     case(marks >= 80 && marks < 90):
+
+//     console.log(`You have out done yourself 80 times over`);
+
+//     break;
+    
+//     default:
+
+//     console.log('There\'s no hope, I\'mm sorry')
+
+//     break;
+
+// }
+
+// ----for loops----
+
+const myCars = [
+    'Toyota',
+    'BMW',
+    'Mercedes-AMG',
+    'Bugatti'
+];
+
+// let myCar = {
+//     make: 'Toyota',
+//     model: 'Corolla',
+//     year: '1996'
+// };
+
+// for (let index = 0; index < myCars.length; index++) {
+//     const element = myCars;
+//     console.log(myCars[3]);
+// }
+
+// for (const key in myCar) {
+//     console.log(key, myCar[key]);
+// }
+
+// for (const y of myCars) {
+//     console.log(y);
+// }
+
+// ---while loops---
+
+// let cnt = 0;
+// while(cnt < 10){
+//     console.log(cnt);
+//     cnt++;
+// }
+// let numbers = [8, 5, 3, 6];
+// do {
+//     console.log(numbers[3]);
+//     cnt++;
+// } while(cnt < numbers.length); 
+
+
+// ************************
+// ----Factory functions----
+// ************************
+
+// let x = 10,
+//     y = 9;
+
+// function add() {
+//     console.log(x + y);
+// }
+// function add() {
+
+// let x = document.getElementById("first").value !== undefined ? document.getElementById("first").value :10;
+// let y = document.getElementById("second").value !== undefined ? document.getElementById("first").value :9;
+
+// console.log (x + +y);
+
+// }
+
+// function add() {
+//     return x + y;
+// }
+
+// console.log(add());
+
+// let person = {
+//     name: 'Jason',
+//     surname: 'Doe',
+//     age: 30
+// }
+
+// let person2 = {
+//     name: 'Jason',
+//     surname: 'Doe',
+//     age: 30
+// }
+
+// function createPeople(name, surname, age) {
+//     return {
+//         name,
+//         surname,
+//         age
+//     }
+// }
+
+// let person1 = createPeople('Keenan', 'Davids', 50);
+// console.log(person1)
+
+// ----constructor functions-----
+
+// function CreatePerson(name, surname, age) {
+//     this.name
+//     this.surname
+//     this.age
+// }
+
+// let person1 = new CreatePerson('Puleng', 'Xolani', 25)
+
+// console.log(person1)
+
